112f21aa5090fe2a729d52fc3b49f6197a107ffb
Light-MR <lmaria.reyes221b@ciencias.unam.mx>
2023-03-06 15:55:46 -0600

    Consulta Id, eliminar

--- src/ArchivoProducto.py
+++ src/ArchivoProducto.py
@@ -1,51 +1,47 @@
 import pandas as pd
-import csv
 import os.path
-import sys
 from Producto import Producto
-from pathlib import Path
 
 
-
-"""Data frame que almacenara los productos existentes"""
-df_producto = pd.DataFrame(columns=['ID', 'Nombre','Stock', 'Marca','Presentacion',
-                                    'Precio', 'Refrigeracion', 'FechaCad', 'FechaLab'])
 
 #Nombre del archivo csv que guardara los productos
 filePath = "src/CSV/Productos.csv"
 
-""" 
-Verifica si el archivo existe
-:return true si el archivo existe, false en otro caso.
-"""
+
+df_producto = pd.DataFrame(columns=['Id', 'Nombre','Stock', 'Marca','Presentacion',
+                                    'Precio', 'Refrigeracion', 'FechaCad', 'FechaLab'])
+
+
+#df_producto.set_index('Id')
+
 def exist_file( ):
-    global fileNam
-    if(os.path.exists(filePath)):
-        return True
-    else:
-        #En otro caso lo escribe
-        return False
+   """ 
+   Verifica si el archivo existe
+   :return true si el archivo existe, false en otro caso.
+   """
+   return os.path.exists(filePath)
 
 
 
 
-""" Lee un archivo csv y lo convierte a un dataframe
-:return dataframe con los datos del archiv"""    
+
 def readCSV():
+    """ Lee un archivo csv y lo convierte a un dataframe
+    :return dataframe con los datos del archiv"""    
     global filePath
-    df = pd.read_csv(filePath,index_col=0) 
+    df = pd.read_csv(filePath, index_col=0) 
     return df
 
 
 
-"""
-Agrega producto a un dataframe existente y actualiza el archivo
-csv
-:param Producto producto: objeto producto a agregar.
-:return dataframe con la informacion actualizada.
-        
- """
-def agregarProducto(producto):   
+
+def agregarProducto(producto):
+    """
+    Agrega producto a un dataframe existente y actualiza el archivo
+    csv
+    :param Producto producto: objeto producto a agregar.
+    :return dataframe con la informacion actualizada.
+    """   
     global df_producto
     global filePath
     dataProducto =[  producto.get_id(),producto.get_nombre(),
@@ -57,58 +53,90 @@ def agregarProducto(producto):
                      producto.get_fechLab()]
     #df_producto = readCSV(file )
    
-    df_producto=pd.concat([df_producto,pd.DataFrame([dataProducto],columns=df_producto.columns)], ignore_index=True)#Añade en la ultima posicion
-    #df.set_index('ID', inplace=True)
-    df_producto.to_csv(filePath)
+    df_producto=pd.concat([df_producto,pd.DataFrame([dataProducto] , index = [producto.get_id()],columns=df_producto.columns)], ignore_index=False)#Añade en la ultima posicion
+    #df_producto.set_index('ID', inplace=True)
+    df_producto.to_csv(filePath, index_label='Id')
     
     
     return df_producto
     
-"""
-Incializa el archivo productos desde 0:
-si no hay productos.
 
-:param Producto producto: Objeto de la clase producto, el cual
-será el primero en almacenar.
-:return dataframe inicializado.
-"""    
-def inicializaProducto(producto):
+def inicializaProducto():
+    """
+    Incializa el archivo productos desde 0:
+    si no hay productos.
+    Genera el archivo Producto.csv
+    """    
     global df_producto
     global filePath
-    dataProducto = [ producto.get_id(),producto.get_nombre(),
-                     producto.get_stock(), producto.get_marca(),
-                     producto.get_presentacion(),
-                     producto.get_precio(),
-                     producto.get_refrigeracion(),
-                     producto.get_fechCad(),
-                     producto.get_fechLab()]
-    new_df= pd.DataFrame([dataProducto], columns=df_producto.columns)
-    df_producto = pd.concat([df_producto, new_df],ignore_index=True)
-    #df.reset_index(drop = True)
-    #df.set_index('ID', inplace=True)
-    #print(df)
-    #genernando el archivo
+    
     df_producto.to_csv(filePath)
-    return df_producto
+
+#El archivo ya debe estar cargado con los IDS como indices
+
+
+def consultaProducto(id):
+    """
+    Consulta un producto proporcionando ID
+    :param str id: Id del producto.
+    :return : regresa el producto corrrespondiente al id.
+    """
+    producto = df_producto.loc[id]
+    return producto
+    
+
+
+#El archivo ya debe estar cargado
+
+def eliminarProducto(id):
+    global df_producto
+    df_producto.drop(index=id, inplace=True)
+    df_producto.to_csv(filePath, index_label='Id')
+    
+
+
+
+
+
+
+#---------------------------------------------------------------
 
 producto = Producto('XX1', 'Verduras', 34, 'Barcel', 'Bolsa', 24, False, '02-04-2023', '04-09-2019')
 producto1 = Producto('XX2', 'Verduras', 54, 'Barcel', 'Lata', 64, False, '02-04-2023', '04-09-2019')
+producto3 = Producto('XX3', 'Leche', 75, 'Santa Clara', 'Carton', 34, False, '02-04-2023', '04-09-2019')
 
 
-
-
+"""
 if exist_file():
     df_producto = readCSV()
-    #df_producto.set_index('ID', inplace=True)
-    df_producto =agregarProducto(producto1)
-    df_producto.to_csv(filePath)
+    df_producto = agregarProducto(producto3)
     print(df_producto)
-else:
-    df_producto = inicializaProducto(producto)
+   
+    
+    print(df_producto.shape[1])
+else: 
+    inicializaProducto()
     print(df_producto)
+    
+    
+"""    
+
+#Probando consulta
+"""
+df_producto = readCSV()
+print('Consulta producto XX3')
+print(consultaProducto('XX3'))
+"""
 
 
 
+#Probando eliminar filas
+df_producto = readCSV()
+print(df_producto)
+print('Eliminando XX2')
+print(eliminarProducto('XX2'))
 
+
+#print(df_producto)
 #df_producto = agregarProducto(producto1,'productosSuc102')
 #print(df_producto)
