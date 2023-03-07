0732805e03ede79b30e942c1dab43bd73a0e7bec
Jonathan318042989 <jonathanmartinezc0910@ciencias.unam.mx>
2023-03-06 18:33:21 -0600

    Clase archivo para sucursal casi terminada

--- src/Archivo.py
+++ src/Archivo.py
@@ -3,12 +3,12 @@ import os.path
 import pandas as pd
 import sys
 from Sucursal import Sucursal
-from Empleado import Empleado
-from Producto import Producto
+
 
 class Archivo:
     
     global archivo
+    sucursalPath = "CSV/Sucursales.csv"
     def __init__(self):
         global archivo
 
@@ -26,8 +26,8 @@ class Archivo:
         archivo.close()
 
     def agregarSucursal(self, archivo, sucursal):
-        with open('CSV/Sucursales.csv', 'a', newline='') as archivo:
-
+        with open(self.sucursalPath, 'a', newline='') as archivo:
+            print(sucursal.getTelefono())
             writer = csv.writer(archivo)
             nuevaSucursal = [sucursal.getID(), sucursal.getNombre(),
                         sucursal.getColonia(), sucursal.getCalle(),
@@ -38,85 +38,81 @@ class Archivo:
         archivo.close()
 
     def consultarSucursal(self, id):
-        with open('CSV/Sucursales.csv') as archivo:
+        data = pd.read_csv(self.sucursalPath, index_col= 'ID')
+        sucursal = data.loc[id]
+        return sucursal
+    
+    def eliminarSucursal(self, id):
+        if(self.existeIDSucursal(id)):
+            data = pd.read_csv(self.sucursalPath)
+            print(id)
+            indice = data.index[data["ID"] == id]
+            data.drop(indice, inplace=True)
+            data.to_csv(self.sucursalPath, index= False) 
+        else:
+            print("No se encontro el ID") 
+
+    def editarDato(self, id,columna, datoNuevo):
+        if(self.existeIDSucursal(id)):
+            data = pd.read_csv(self.sucursalPath, index_col= 'ID')
+            data.loc[id, columna]  = datoNuevo
+            data.to_csv(self.sucursalPath)
+        else:
+            print("No se encontro el ID")
+
+    def editarListaTelefonos(self, id, opcion, datoNuevo):
+        if(self.existeIDSucursal(id)):
+            if(opcion == 1):
+                data = pd.read_csv(self.sucursalPath, index_col= 'ID')
+                datoActual = data.loc[id, "Telefonos"]
+                listaActual = datoActual.strip('][').split(', ')
+                listaActual = list(map(int, listaActual))
+                if(type(datoNuevo == int)):
+                    listaActual.append(datoNuevo)
+
+                    data.loc[id, "Telefonos"]  = str(listaActual)
+                    data.to_csv(self.sucursalPath)
+                else:
+                    print("El telefono debe ser un numero")
+            elif(opcion == 2):
+                data = pd.read_csv(self.sucursalPath, index_col= 'ID')
+                datoActual = data.loc[id, "Telefonos"]
+                listaActual = datoActual.strip('][').split(', ')
+                listaActual = list(map(int, listaActual))
+                del listaActual[datoNuevo]
+                data.loc[id, "Telefonos"]  = str(listaActual)
+                data.to_csv(self.sucursalPath)
+        else:
+            print("No se encontro el ID")
+
+    def existeIDSucursal(self,id):
+        with open(self.sucursalPath) as archivo:
             reader = csv.reader(archivo, delimiter=',')
             encabezado = next(archivo)
             next(archivo)
             for row in reader:
                 if (row[0] == id):
-                    print(encabezado)
-                    print(row)
-                    break
-    
-    def eliminarSucursal(self, id):
-        if(self.existeIDSucursal(id)):
-            data = pd.read_csv('CSV/Sucursales.csv')
-            print(id)
-            indice = data.index[data["ID"] == id]
-            data.drop(indice, inplace=True)
-            data.to_csv('CSV/Sucursales.csv', index= False) 
-        else:
-            print("No se encontro el ID") 
-
-    def editarDato(self, id,columna, datoNuevo):
-        data = pd.read_csv('CSV/Sucursales.csv', index_col= 'ID')
-        data.loc[id, columna]  = datoNuevo
-        data.to_csv('CSV/Sucursales.csv', index= False)      
-
+                    return True
+            return False
         
             
 
 arch = Archivo()
 arch.verificaExistenciaSucursales("Sucursales")
 """
-suc2 = Sucursal()
-suc2.setID("sucursal01")
-suc2.setNombre("Sucursal 01")
-suc2.setColonia("Oriental")
-suc2.setCalle("Oriente 45")
-suc2.setNumero(78)
-suc2.setCodigoPostal("12345")
-suc2.setTelefono(5598546451)
-suc2.setTelefono(1234569426)
-suc2.setTelefono(6489278922)
-suc1 = Sucursal()
-suc1.setID("sucursal02")
-suc1.setNombre("Sucursal 02")
-suc1.setColonia("Providencia")
-suc1.setCalle("Oriente 23")
-suc1.setNumero(78)
-suc1.setCodigoPostal("12345")
-suc1.setTelefono(1234567894)
-suc1.setTelefono(1234568935)
-suc1.setTelefono(6489278922)
-suc = Sucursal()
-suc.setID("sucursal03")
-suc.setNombre("Sucursal 03")
-suc.setColonia("Agricola")
-suc.setCalle("Oriente 45")
-suc.setNumero(78)
-suc.setCodigoPostal("12345")
-suc.setTelefono(5598546451)
-suc.setTelefono(1234569426)
-suc.setTelefono(6489278922)
-suc3 = Sucursal()
-suc3.setID("sucursal04")
-suc3.setNombre("Sucursal 04")
-suc3.setColonia("Doctores")
-suc3.setCalle("Oriente 23")
-suc3.setNumero(78)
-suc3.setCodigoPostal("12345")
-suc3.setTelefono(1234567894)
-suc3.setTelefono(1234568935)
-suc3.setTelefono(6489278922)
-arch.agregarSucursal(arch.archivo, suc2)
-arch.agregarSucursal(arch.archivo, suc1)
-arch.agregarSucursal(arch.archivo, suc)
-arch.agregarSucursal(arch.archivo, suc3)
-arch.consultarSucursal("sucursal02")
-arch.eliminarSucursal("sucursal02")
-arch.editarDato("sucursal03", "Colonia", "Roma")
+suc1 = Sucursal("sucursal01", "Sucursal 01", "Agricola", "249", 78, "08500", 55137890, "12-12-2012")
+print(suc1.getTelefono())
+arch.agregarSucursal(arch, suc1)
+suc2 = Sucursal("sucursal02", "Sucursal 02", "Roma", "265", 20, "09420", 8, "12-12-2020")
+print(suc2.getTelefono())
+arch.agregarSucursal(arch, suc2)
+suc3 = Sucursal("sucursal03", "Sucursal 03", "Doctores", "789", 52, "89520", 2981236, "12-12-2016")
+print(suc3.getTelefono())
+arch.agregarSucursal(arch, suc3)
+suc4 = Sucursal("sucursal04", "Sucursal 04", "Agricola", "456", 120, "36250", 1981236, "12-02-2023")
+print(suc4.getTelefono())
+arch.agregarSucursal(arch, suc4)
+arch.editarListaTelefonos("sucursal02",  2,  0)
 """
+print(arch.consultarSucursal("sucursal02"))
 
-arch.eliminarSucursal("sucursal04")
-arch.editarDato("sucursal03", "Nombre", "Roma")
